a2c5620d1b8ed2d48d3e5f70f103fbc202c38fb2
Lauri <lauri@icantfeelmylegs.com>
2019-10-01 19:13:57 +0300

    - expose `title` and `style` in `Div`

--- src/components/Div.js
+++ src/components/Div.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import classsnames from 'classnames'
-import { func, arrayOf, oneOfType, node } from 'prop-types'
+import { func, arrayOf, oneOfType, node, string } from 'prop-types'
 
-const Div = ({ children, onClick, ...rest }) => (
-  <div onClick={onClick} className={classsnames(rest)}>
+const Div = ({ children, onClick, title, style, ...rest }) => (
+  <div className={classsnames(rest)}
+    {...{
+      ...(title && { title }),
+      ...(onClick && { onClick }),
+      ...(style && { style }),
+    }}
+  >
     {children}
   </div>
 )
 
 const propTypes = {
   onClick: func,
+  title: string,
+  style: string,
   children: oneOfType([arrayOf(node), node]),
 }
 Div.propTypes = propTypes
 
 const defaultTypes = {
   onClick: null,
+  title: null,
+  style: null,
   children: null,
 }
 Div.defaultTypes = defaultTypes
